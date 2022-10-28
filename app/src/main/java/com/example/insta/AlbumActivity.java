3fe0bba8dd181453fde49dff9aa0c6c73401e98e
TheBlackIgor <swierczynskigor@gmail.com>
2022-10-28 10:39:22 +0200

    images

--- app/src/main/java/com/example/insta/AlbumActivity.java
+++ app/src/main/java/com/example/insta/AlbumActivity.java
@@ -1,21 +1,25 @@
 package com.example.insta;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import java.io.File;
-import java.util.Arrays;
 
 public class AlbumActivity extends AppCompatActivity {
 
@@ -39,29 +43,30 @@ public class AlbumActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_album);
 
-        File pic = Environment.getExternalStoragePublicDirectory( Environment.DIRECTORY_PICTURES );
-        File dir = new File(pic, "swierczynski");
-        pic = Environment.getExternalStoragePublicDirectory( Environment.DIRECTORY_PICTURES + "/swierczynski" );
-        dir = new File(pic, "folder1"); dir.mkdir();
-        dir = new File(pic, "folder2"); dir.mkdir();
-        dir = new File(pic, "folder3"); dir.mkdir();
-        File[] files = pic.listFiles();
-        Arrays.sort(files); // sortowanie plików wg nazwy
-        Log.d("folders", "=====================\n\n" + files.toString() +"\n\n=====================");
-        for (File file : pic.listFiles()){
-            Log.d("file: ", file.getName());
-        }// tablica przechowująca testowe dane
+        File pic = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File dir = new File(pic, "RyszkaJakub");
+        dir.mkdir();
+        File dir1 = new File(pic, "RyszkaJakub/ludzie");
+        dir1.mkdir();
+        dir1 = new File(pic, "RyszkaJakub/rzeczy");
+        dir1.mkdir();
+        dir1 = new File(pic, "RyszkaJakub/miejsca");
+        dir1.mkdir();
+
+        File[] files = dir.listFiles();
+        for(File file : dir.listFiles()){
+            Log.d("TAG", "Nr klikanego wiersza : " + file.toString());
+        }
 
         ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
         listView = findViewById(R.id.listViewID);
         refreshDirectory(dir);
 
-        File finalPic = pic;
-        File finalDir = dir;
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -72,9 +77,9 @@ public class AlbumActivity extends AppCompatActivity {
                 alert.setPositiveButton("USUŃ", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int which) {
-                        File toRemove = new File(finalPic, adapterView.getItemAtPosition(i).toString());
+                        File toRemove = new File(pic, "RyszkaJakub/" + adapterView.getItemAtPosition(i).toString());
                         toRemove.delete();
-                        refreshDirectory(finalDir);
+                        refreshDirectory(dir);
                     }
                 });
                 alert.setNegativeButton("Anuluj", new DialogInterface.OnClickListener() {
@@ -87,5 +92,55 @@ public class AlbumActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        addFolderBtn = findViewById(R.id.addButton);
+        addFolderBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(AlbumActivity.this);
+                alert.setTitle("Nowy folder");
+                alert.setMessage("Podaj nazwe nowego folderu");
+                EditText input = new EditText(AlbumActivity.this);
+                input.setText("Nowy folder");
+                alert.setView(input);
+                alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        File pic = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+                        File dir1 = new File(pic, "RyszkaJakub/" +  input.getText());
+                        dir1.mkdir();
+                        refreshDirectory(dir);
+                    }
+                });
+                alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+
+                    }
+                });
+
+                alert.show();
+            }
+
+        });
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(AlbumActivity.this, MediaStore.Images.class);
+                intent.putExtra("folder", adapterView.getItemAtPosition(i).toString());
+                startActivity(intent);
+            }
+        });
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        if(item.getItemId() == android.R.id.home){
+            this.finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
