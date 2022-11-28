0c6ad9e944b8446c694f8d03e1fc6e24e28617db
Igor Świerczyński <swierczynskigor@gmail.com>
2022-11-28 14:29:52 +0100

    fix and new fucking problems

---
+++ app/src/main/java/com/example/insta/ImageActivity.java
@@ -0,0 +1,18 @@
+package com.example.insta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+
+public class ImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+    }
+
+
+}
--- app/src/main/java/com/example/insta/ImagesActivity.java
+++ app/src/main/java/com/example/insta/ImagesActivity.java
@@ -1,22 +1,56 @@
 package com.example.insta;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+import java.io.File;
 
 public class ImagesActivity extends AppCompatActivity {
+
+    private LinearLayout imagesContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_images);
+
+        this.imagesContainer = (LinearLayout) findViewById(R.id.imagesContainer);
+
         String folder = getIntent().getStringExtra("folder");
         setTitle(folder);
-
         ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
+        File pic = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File dir = new File(pic, "swierczynski/" + folder);
+        dir.mkdir();
+        Log.d("images", "Amount of photos: "+dir.listFiles().length);
+    }
 
+    private Bitmap betterImageDecode(String filePath) {
+        Bitmap myBitmap;
+        BitmapFactory.Options options = new BitmapFactory.Options();    //opcje przekształcania bitmapy
+        options.inSampleSize = 4; // zmniejszenie jakości bitmapy 4x
+        //
+        myBitmap = BitmapFactory.decodeFile(filePath, options);
+        return myBitmap;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            this.finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
